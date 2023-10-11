86095c714dbb3e1a6d4e0bd231d0240ed8491acc
ZennDev1337 <zenndev@protonmail.com>
2023-10-11 17:43:20 +0200

    new docs and new readme

---
+++ docs/doc/arduboy_rust/arduboy2_library/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["FONT_HEIGHT","FONT_WIDTH","HEIGHT","WIDTH"],"enum":["Color"],"mod":["sprites"],"struct":["Arduboy2","Point","Rect"]};
---
+++ docs/doc/arduboy_rust/arduino_system/serial_print/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["available","begin","end","print","println","read","read_as_utf8_str"],"trait":["Serialprintable","Serialprintlnable"]};
